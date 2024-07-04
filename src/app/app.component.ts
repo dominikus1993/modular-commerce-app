3125ab2de88f228ff436a3df4879a5f04bdc1a9f
Dominik.Kotecki <dominikus1910@gmail.com>
2024-07-04 18:25:43 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from "./test/test.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, Signal, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
+  public client = inject(HttpClient)
   public title: string = "Test Component"
   @Input("message") public message: string = "Hello World"
+
+  textInput: string = ""
+  public count = signal(0)
+
+  public increment(): void {
+    this.count.update(x => x + 1)
+  }
+
+  public decrement(): void {
+    this.count.update(x => x - 1)
+  }
+
+  public send() {
+    this.title = this.textInput
+    this.textInput = ""
+  }
 }
