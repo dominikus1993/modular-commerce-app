35b5a14ebb07b97defdb83a6116741b921ce6fa5
Dominik.Kotecki <dominikus1910@gmail.com>
2024-07-26 18:34:05 +0200

    fix

---
+++ src/app/product-list/product-list-element/product-list-element.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../shared/types';
+
+@Component({
+  selector: 'app-product-list-element',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list-element.component.html',
+  styleUrl: './product-list-element.component.scss'
+})
+export class ProductListElementComponent {
+  @Input({required: true}) product!: Product;
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductListElementComponent } from "./product-list-element/product-list-element.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductListElementComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
