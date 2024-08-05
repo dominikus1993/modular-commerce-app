519e646783858d2e04a83a2912df690f394ea667
Dominik.Kotecki <dominikus1910@gmail.com>
2024-08-05 18:27:28 +0200

    add observables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideServerRendering(), { provide: "BASE_API_URL", useValue: process.env["BASE_API_URL"] ?? "http://localhost:5011"}, provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideServerRendering(), { provide: "BASE_API_URL", useValue: environment.BASE_API_URL}, provideAnimationsAsync()]
 };
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output, Signal, computed, inject } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, Signal, computed, inject } from '@angular/core';
 import { ProductListElementComponent } from "./product-list-element/product-list-element.component";
 import { GetProductsResult, ProductListService } from './shared/product-list-service';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FilterMetadata } from './shared/types';
 import { ProductListElementsComponent } from "./product-list-elements/product-list-elements.component";
 import {MatPaginatorModule, PageEvent} from '@angular/material/paginator'
+import { AsyncPipe } from '@angular/common';
 
 export interface ProductsInfo {
   metadata: FilterMetadata | null;
@@ -19,7 +20,7 @@ export type PageSize = 12 | 24 | 48 | 96;
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ProductListElementComponent, ProductListElementsComponent, MatPaginatorModule],
+  imports: [ProductListElementComponent, ProductListElementsComponent, MatPaginatorModule, AsyncPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -28,30 +29,8 @@ export class ProductListComponent {
   public pageSize: PageSize = 12;
   public page: number = 1;
 
-  products = toSignal(this.getProducts(), {initialValue: { kind: "ok", value: { products: [], metadata: null, count: 0, total: 0 } }});
+  products$: Observable<GetProductsResult> = this.getProducts()
 
-  isSuccess = computed(() => this.products().kind === "ok");
-
-  productsListResult = computed(() => {
-    const result = this.products();
-    if (result.kind === "ok") {
-      return result.value;
-    }
-    return { products: [], metadata: null, count: 0, total: 0 };
-  });
-
-  productsList = computed(() => {
-    const result = this.productsListResult();
-    return result.products;
-  });
-
-  errorMessage = computed(() => {
-    const result = this.products();
-    if (result.kind === "error") {
-      return result.error;
-    }
-    return "";
-  });
 
   getProducts() {
     return this.productsService.getProducts(null, this.page, this.pageSize);
@@ -59,5 +38,6 @@ export class ProductListComponent {
 
   onChangePage(event: PageEvent) {
     console.log(event.pageIndex);
+    console.log(event.pageSize);
   }
 }
